64c6d7dbf912887fb1a12d4ff694f457aa4efd7f
Aakash9399 <akashpalliwar79@gmail.com>
2023-08-02 11:30:52 +0530

    Intro to intersection

---
+++ setintersection.py
@@ -0,0 +1,14 @@
+n_eng=int(input("enter the number"))
+
+
+english=set()
+french=set()
+
+for i in range(n_eng):
+    english.add(int(input("enter the number of student")))
+
+n_french=int(input("enter the number"))
+for j in range(n_french):
+    french.add(int(input("enter the numbe of student")))
+res=english.intersection(french)  
+print(len(res))
